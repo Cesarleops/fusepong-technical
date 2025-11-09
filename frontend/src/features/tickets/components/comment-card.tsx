59005d647467e4f18f5a2d31fe6fa1466f3bacde
cesarleyton <cesarleyton549@gmail.com>
2025-11-09 08:04:35 -0500

    format ticket comment date

--- frontend/src/features/tickets/components/comment-card.tsx
+++ frontend/src/features/tickets/components/comment-card.tsx
@@ -1,3 +1,4 @@
+import { formatDate } from "@/lib/utils";
 import type { TicketComment } from "../types";
 
 interface Props {
@@ -5,11 +6,12 @@ interface Props {
 }
 
 export const TicketCommentCard = ({ comment }: Props) => {
+  const createdAt = formatDate(comment.created_at);
   return (
     <article className="hover:bg-sidebar-accent hover:text-sidebar-accent-foreground flex flex-col items-start gap-2 border-b p-4 text-sm leading-tight whitespace-nowrap last:border-b-0">
       <div className="flex w-full items-center gap-2">
-        <span>{comment.author.name}</span>{" "}
-        <span className="ml-auto text-xs">{comment.created_at}</span>
+        <span>{comment.author.name}</span>
+        <span className="ml-auto text-xs">{createdAt}</span>
       </div>
       <span className="line-clamp-2 w-[260px] text-xs whitespace-break-spaces">
         {comment.comment}
